19b4601326dfde2d360bc3091b2e280e32cab783
Ye Yint Aung <mryeyintaung669@gmail.com>
2024-12-13 23:58:07 +0630

    fzf is required

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -9,7 +9,7 @@ return {
     "LazyVim/LazyVim",
     opts = {
       --colorscheme = "tokyonight-moon",
-      colorscheme = "monokai-pro-spectrum"
+      colorscheme = "monokai-pro-classic"
     },
   },
 }
